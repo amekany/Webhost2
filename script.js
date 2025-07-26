db54931722cfa8f0030be82cd7e20fccbc91e9ad
amekany <amekanyltd@gmail.com>
2025-07-26 11:24:24 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Menu toggle script
+const burger = document.getElementById('burger');
+const navLinks = document.getElementById('nav-links');
+
+burger.addEventListener('click', () => {
+      navLinks.classList.toggle('active');
+});
+})
